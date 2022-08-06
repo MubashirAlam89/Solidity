12a270e68b9f81f75115a5af60ff578bc5b1c36d
mubashiralam89 <mubashiralam89@gmail.com>
2022-08-07 01:24:14 +0500

    intialized and declared a state variable

--- Contract-1-Todo-App/todoContract.sol
+++ Contract-1-Todo-App/todoContract.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract TodoList {
-   
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+  
 }
