e08c0ab609c8b9212e50e14ad9aa16f369d63219
mubashiralam89 <mubashiralam89@gmail.com>
2022-08-08 01:27:36 +0500

    created a toggleCompleted function

--- Contract-1-Todo-App/todoContract.sol
+++ Contract-1-Todo-App/todoContract.sol
@@ -24,6 +24,11 @@ contract TodoList {
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
     }
-
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 
 }
